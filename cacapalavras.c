1733b99ff03e260259a7c45a2b81b4e1b89d9992
CDGodoy <cdgodoy17@gmail.com>
2021-03-21 22:21:39 -0300

    finalizando o dia

--- cacapalavras.c
+++ cacapalavras.c
@@ -25,9 +25,10 @@ int contalinhas(char *pesq){
 
 }
 
+
 int main(){
     
-    const int MAX = 173; //A maior palavra do mundo tem 173 letras
+    const int MAX = 172; //A maior palavra do mundo tem 173 letras
     char pesq[MAX];
     printf("Nome do arquivo a ser pesquisado: ");
     scanf("%s", pesq);
@@ -35,13 +36,16 @@ int main(){
     int nlinhas = contalinhas(pesq);
  
     FILE *arq;
-    int i, j, tam;
+    FILE *uarq;
+    //FILE *tarq;
+    int i, j, k, tam, tam1;
     char palavras[nlinhas][MAX];
     char buffer[nlinhas][MAX];
     char *resul;
-
     char dicpalavra[MAX];
 
+    //tarq = fopen("meutexto.txt", "a+b");
+    uarq = fopen(pesq, "r");
     arq = fopen("dic.txt", "r");
 
     while (!feof(arq)){
@@ -49,8 +53,24 @@ int main(){
         tam = strlen(dicpalavra);
         for (i=0; i<tam; i++){ //Convertendo palavras do dic em CapsLock
             dicpalavra[i] = toupper(dicpalavra[i]); 
-        }
-        
+        } 
     }
+        //     for(i=0;i<nlinhas;i++){ //Mostra todas as palavras do arquivo
+        //     resul = fgets(*buffer,MAX,uarq); //(ponteiro que irá armazenar, tamanho máximo da palavra, arquivo destino)
+        //     strcpy(palavras[i], *buffer); //Copiando do ponteiro que armazenou para o vetor de palavras
+        //     tam1 = strlen(palavras);
+        //     for (k=0; k<nlinhas; k++){ //Convertendo palavras do arquivo em CapsLock
+        //         //printf("linha: %d\n", k);
+        //         for(j=0;j<tam1; j++){
+        //             palavras[k][j] = toupper(palavras[k][j]); 
+        //             //printf("COLUNA: %d\n", j);
+        //         }
+        //         printf("%s", palavras);
+
+        //     }
+            
+        //     //fputs(palavras,tarq);
+
+        // }
 
 }
