9943dff91421f86ee7bc6c45cce38f91bba3b3b4
CDGodoy <cdgodoy17@gmail.com>
2021-03-22 15:48:34 -0300

    Codigo limpo e reorganizado

--- cacapalavras.c
+++ cacapalavras.c
@@ -1,141 +1,103 @@
 #include <stdio.h>
+
 #include <string.h>
 
 
+int contalinhas(char * pesq) {
+  FILE * arqCL; //Arquivo conta linhas
 
-int contalinhas(char *pesq){
-    FILE *arqCL; //Arquivo conta linhas
+  char c, letra = '\n';
+  int contaLinha;
 
-    char c, letra = '\n'; 
-    int contaLinha;
-
-    arqCL = fopen(pesq, "r"); //Abrindo arquivo solicitado pelo usuário
-    if(arqCL != NULL){
-    while(fread (&c, sizeof(char), 1, arqCL)) {
-        if(c == letra) {
-            contaLinha++;
-        }
-    } 
-    }else{
-        printf("Erro no arquivo");
-        return 0;
+  arqCL = fopen(pesq, "r"); //Abrindo arquivo solicitado pelo usuário
+  if (arqCL != NULL) {
+    while (fread( & c, sizeof(char), 1, arqCL)) {
+      if (c == letra) {
+        contaLinha++;
+      }
     }
-    fclose(arqCL);
-    return (contaLinha+1); //+1 pois a quantidade de \n é igual a linhas-1
+  } else {
+    printf("Erro no arquivo");
+    return 0;
+  }
+  fclose(arqCL);
+  return (contaLinha + 1); //+1 pois a quantidade de \n é igual a linhas-1
 
 }
 
+int main() {
 
-int main(){
-    
-    const int MAX = 172; //A maior palavra do mundo tem 173 letras
-    char pesq[MAX];
-    printf("Nome do arquivo a ser pesquisado: ");
-    scanf("%s", pesq);
+  const int MAX = 172; //A maior palavra do mundo tem 173 letras
+  char pesq[MAX];
+  printf("Nome do arquivo a ser pesquisado: ");
+  scanf("%s", pesq);
 
-    int nlinhas = contalinhas(pesq);
- 
-    FILE *arq;
-    FILE *uarq;
-    FILE *tarq;
-    int i, j, k, tam, tam1, l=0, contador = 0, Linha =1;
-    char palavras[MAX][MAX];
-    char buffer[nlinhas][MAX];
-    char resul[nlinhas];
-    char dicpalavra[MAX];
-    char ch;
-    int qt=0;
-    tarq = fopen("meutexto.txt", "w");
-    uarq = fopen(pesq, "r");
-    if (uarq == NULL){
-        printf("Erro ao abrir o arquivo");
-        return(0);
+  int nlinhas = contalinhas(pesq);
+
+  FILE * arq;
+  FILE * uarq;
+  FILE * tarq;
+  int i, j, k, tam, tam1, l = 0, contador = 0;
+  char palavras[MAX][MAX];
+  char buffer[nlinhas][MAX];
+  char resul[nlinhas];
+  char dicpalavra[MAX];
+  char ch;
+  int qt = 0;
+  tarq = fopen("meutexto.txt", "w");
+  uarq = fopen(pesq, "r");
+  if (uarq == NULL) {
+    printf("Erro ao abrir o arquivo");
+    return (0);
+  }
+  arq = fopen("dic.txt", "r");
+
+  while (!feof(arq)) {
+    fgets(dicpalavra, MAX, arq);
+    dicpalavra[strlen(dicpalavra) - 1] = dicpalavra[strlen(dicpalavra)]; //Retira o \0 do fgets
+    tam = strlen(dicpalavra);
+    for (i = 0; i < tam; i++) { //Convertendo palavras do dic em CapsLock
+      dicpalavra[i] = toupper(dicpalavra[i]);
     }
-    arq = fopen("dic.txt", "r");
+    qt = 0;
 
-    while (!feof(arq)){
-        fgets(dicpalavra, MAX, arq);
-        dicpalavra[strlen(dicpalavra)-1] = dicpalavra[strlen(dicpalavra)]; //Retira o \0 do fgets
-        tam = strlen(dicpalavra);
-        for (i=0; i<tam; i++){ //Convertendo palavras do dic em CapsLock
-            dicpalavra[i] = toupper(dicpalavra[i]); 
-        }
-        qt = 0;
-        //printf("%s", dicpalavra);        
-        //dicpalavra é a palavra a ser pesquisada no texto
-        uarq = fopen(pesq, "r");
-        if (uarq == NULL){
-        printf("Erro ao abrir o arquivo");
-        return(0);
+    //dicpalavra é a palavra a ser pesquisada no texto
+
+    uarq = fopen(pesq, "r"); //Abrindo o arquivo dentro do ciclo, para que ele SEMPRE volte a linha inicial para rodar novamente o texto
+    if (uarq == NULL) {
+      printf("Erro ao abrir o arquivo");
+      return (0);
     }
-        while(fscanf(uarq, "%s",palavras[contador]) != EOF && fscanf(uarq,"%c",&ch)!=EOF){
-            if(ch=='\n')Linha++;
-            tam1 = strlen(palavras[contador]);
-            for (i=0; i<tam1; i++){ //Convertendo palavras do dic em CapsLock
-                palavras[contador][i] = toupper(palavras[contador][i]); 
-            }
-            if (strcmp(palavras[contador],dicpalavra)==0){
-                qt++;
-                printf("A palavra %s está no texto\n", palavras[contador]);
-            }else
-                contador++;
-        }
-        fclose(uarq);
-        printf("A palavra %s foi encontrada %d no texto\n", dicpalavra,qt);
 
+    while (fscanf(uarq, "%s", palavras[contador]) != EOF && fscanf(uarq, "%c", & ch) != EOF) {
 
-        // while (!feof(uarq)){
-        //     for (i=0; i<nlinhas; i++){
- 
-        //         fgets(*buffer, MAX, uarq);
-        //         strcpy(palavras[i], *buffer);
-        //         tam1 = strlen(palavras[i]);
-        //         for (j=0; j<tam1; j++){
-        //             palavras[i][j] = toupper(palavras[i][j]);
-                    
-        //         }
+      tam1 = strlen(palavras[contador]); //RETIRANDO CARACTERES DO FINAL DO TEXTO
+      if (palavras[contador][tam1] == '.' || palavras[contador][tam1] == ',' ||
+        palavras[contador][tam1] == ';' || palavras[contador][tam1] == ':' ||
+        palavras[contador][tam1] == '?' || palavras[contador][tam1] == '-') {
+        palavras[contador][strlen(palavras) - 1] = palavras[contador][strlen(palavras)];
+      } //FIM RETIRANDO CARACTERES DO FINAL DO TEXTO
 
+      for (i = 0; i < tam1; i++) { //Convertendo palavras do dic em CapsLock
+        palavras[contador][i] = toupper(palavras[contador][i]);
+      } //FIM CONVERTENDO PALAVRAS DO DIC EM CAPSLOCK
 
-
-        //         // printf("dicpalavra = %s", dicpalavra);
-        //         // printf("\npalavras[i] %s", palavras[i]);
-        //         // if (strstr(palavras[i],dicpalavra)){
-        //         //     printf("----------verdadeiro-------------\n");
-        //         //     l++;
-
-        //         //     //fprintf(tarq,"%s %d\n",dicpalavra,l);
-        //         // }
-        //         //printf("%s", palavras[i]);
-                
-        //     }
-            
-        //     //printf("%s", buffer);
-
-        // }
-
-        //fputs(dicpalavra, tarq); //Escrevendo em outro arquivo com tudo em maiúsculo
-        
+      if (strcmp(palavras[contador], dicpalavra) == 0) {
+        qt++;
+        printf("A palavra %s está no texto\n", palavras[contador]);
+      } else
+        contador++;
     }
-        //     for(i=0;i<nlinhas;i++){ //Mostra todas as palavras do arquivo
-        //     resul = fgets(*buffer,MAX,uarq); //(ponteiro que irá armazenar, tamanho máximo da palavra, arquivo destino)
-        //     strcpy(palavras[i], *buffer); //Copiando do ponteiro que armazenou para o vetor de palavras
-        //     tam1 = strlen(palavras);
-        //     for (k=0; k<nlinhas; k++){ //Convertendo palavras do arquivo em CapsLock
-        //         //printf("linha: %d\n", k);
-        //         for(j=0;j<tam1; j++){
-        //             palavras[k][j] = toupper(palavras[k][j]); 
-        //             //printf("COLUNA: %d\n", j);
-        //         }
-        //         printf("%s", palavras);
+    fclose(uarq); //Fechando o arquivo dentro do ciclo, para que ele SEMPRE volte a linha inicial para rodar novamente o texto
 
-        //     }
-            
-        //     //fputs(palavras,tarq);
+    memset(palavras, 0, sizeof(palavras)); //Resetando vetor palavras
+    contador = 0; //Redefinindo contador
 
-        // }
+    printf("A palavra %s foi encontrada %d no texto\n", dicpalavra, qt);
+    memset(dicpalavra, 0, sizeof(dicpalavra));
 
+  }
 
-    fclose(arq);
-    fclose(uarq);
-    fclose(tarq);
+  fclose(arq);
+  fclose(tarq);
 }
